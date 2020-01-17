2c5742cf1250586b4fbdda8b9a1845c2eaf4d80b
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-17 19:41:08 +0300

    corrected on spacing and indentation

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -6,32 +6,33 @@ import 'rxjs/add/operator/map';
   providedIn: 'root'
 })
 export class GithubService {
-  
+
   private username = 'chepkiruibetty';
   private client_id = '5ffa7c5cd4df559a6ac3';
   private client_secret = 'c928803011adaf70ce872702dc01bc75ef47e6ea';
 
-  
 
 
 
- 
 
 
-getUser(){
+
+
+  getUser() {
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
-    .map(res => console.log(res));
-};
+      .map(res => console.log(res));
+  };
 
-  getRepos(){
-  return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
-    .map(res =>res);
+  getRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+      .map(res => res);
   }
 
-  updateUsername(username: string){
-  this.username = username; }
-  
-  constructor(private http:HttpClient){
+  updateUsername(username: string) {
+    this.username = username;
+  }
+
+  constructor(private http: HttpClient) {
     console.log("Github Service Init...");
   }
 }
