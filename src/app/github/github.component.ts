b2654ca450a6504af74bb6d2c4d22767a8fcf323
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-17 11:12:33 +0300

    add console.log to github component ts

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+  constructor() {
+  console.log('Github Component Init...');
+   }
+  
+
+  ngOnInit() {
+  }
+
+}
