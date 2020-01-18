dd36a414c67303157a7b4fe698c14a4f61525fb1
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-18 20:13:27 +0300

    removed private word from the keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
 };
 export class GithubService {
-  private username = 'chepkiruibetty';
-  private client_id = '5ffa7c5cd4df559a6ac3';
-  private client_secret = 'c928803011adaf70ce872702dc01bc75ef47e6ea';
+ username = 'chepkiruibetty';
+  client_id = '5ffa7c5cd4df559a6ac3';
+  client_secret = 'c928803011adaf70ce872702dc01bc75ef47e6ea';
 }
 /*
  * For easier debugging in development mode, you can import the following file
